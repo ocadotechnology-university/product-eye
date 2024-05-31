b448590038e8f1ae79442b441349bcd6408bf1a9
Karol Charchut <59970980+Quarol@users.noreply.github.com>
2024-05-31 16:02:41 +0200

    Forgot to add sidebar last commit

--- src/SideBar.tsx
+++ src/SideBar.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import { Aside, Header, Product, ProductSpan } from './styles';
+import { Scrollbar } from 'react-scrollbars-custom';
 
 
 const renderProducts = (products: { productName: string; fileName: string }[], onProductSelect: (arg0: string) => void) => {
@@ -20,10 +21,10 @@ const SideBar = ({ products, onProductSelect }: { products: { productName: strin
           <h1>Product-Eye</h1>
         </Header>
 
-        <section>
-          <h2>Products</h2>
-            {renderProducts(products, onProductSelect)}
-        </section>
+        <h2>Products</h2>
+        <Scrollbar style={{ flex: 1, width: "100%", height: "100%" }}>
+          {renderProducts(products, onProductSelect)}
+        </Scrollbar>
       </Aside>
     </>
   );
