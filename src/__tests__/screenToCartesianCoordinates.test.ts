2546620bad4bef0a6bdd06aa47269d2547305805
Karol Charchut <59970980+Quarol@users.noreply.github.com>
2024-05-31 16:30:33 +0200

    Tests to Coordinatertansfromer added

---
+++ src/__tests__/screenToCartesianCoordinates.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, it } from 'vitest';
+import  CooordinateTransformer from '../CoordinateTransformer';
+
+
+describe('Screen coordinates to cartesian coordinates', () => {
+     it('screenToCartesianCoordinates converts correctly', () => {
+       const { x, y } = CooordinateTransformer.screenToCartesianCoordinates(100, 200, 800, 600);
+       expect(x).toBe(-300);
+       expect(y).toBe(100);
+     });
+   });
+  
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejsv/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis'
+  },
+  test: {
+    globals: true,
+    watch: false,
+    environment: 'jsdom',
+    css: true,
+    setupFiles: './src/setupTests.ts'
+  }
 })
