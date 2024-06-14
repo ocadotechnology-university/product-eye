6db9e851b363cef65598192e35adc03f44403068
Karol Charchut <59970980+Quarol@users.noreply.github.com>
2024-06-14 18:37:45 +0200

    EyeTracker refactor + automatic launching of app

--- src/EyeTracker.tsx
+++ src/EyeTracker.tsx
@@ -3,39 +3,28 @@ import { Coordinates } from './Coordinates';
 
 const EyeTracker = () => {
     useEffect(() => {
-        webgazer.showVideoPreview(true)
+        webgazer.showVideoPreview(true);
         webgazer
-        .setRegression('ridge')
-        .saveDataAcrossSessions(true)
-        .showFaceOverlay(false)
-        .showFaceFeedbackBox(true)
-        .showPredictionPoints(true)
-        .applyKalmanFilter(true)
+            .setRegression('ridge')
+            .saveDataAcrossSessions(true)
+            .showFaceOverlay(false) 
+            .showFaceFeedbackBox(true)
+            .showPredictionPoints(true)
+            .applyKalmanFilter(true);
 
-        webgazer.setGazeListener((data, elapsedTime) => {
-            if (data == null) return;
+        const gazeListener = (data, elapsedTime) => {
+            if (data == null) 
+                return;
             Coordinates.setCurrentCoordinates(data.x, data.y);
-        }).begin();
-        
-        
-        /*
-        // NEW CODE
-        webgazer.setRegression('ridge')
-        //.setTracker('clmtrackr')
-        .setGazeListener(function(data, clock) {
-            if (data == null) return;
-                Coordinates.setCurrentCoordinates(data.x, data.y);
-        })
-        .saveDataAcrossSessions(true)
-        .showFaceOverlay(false)
-        .showFaceFeedbackBox(true)
-        .showPredictionPoints(true)
-        .showVideoPreview(true)
-        .applyKalmanFilter(true)
-        .begin();
-        */
+        };
 
-    }, []); // Empty dependency array to run once on mount
+        webgazer.setGazeListener(gazeListener).begin();
+
+        return () => {
+            webgazer.removeGazeListener(gazeListener);
+            webgazer.end();
+        };
+    }, []);
 
     return null;
 };
