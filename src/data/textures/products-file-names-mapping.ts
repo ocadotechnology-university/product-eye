27c6cb10006775f785074d895e3d9162dcf067f7
Karol Charchut <59970980+Quarol@users.noreply.github.com>
2024-06-20 13:25:02 +0200

    Update products-file-names-mapping.ts

--- src/data/textures/products-file-names-mapping.ts
+++ src/data/textures/products-file-names-mapping.ts
@@ -16,29 +16,13 @@ const productsFileNamesMapping = [
         "fileName": "forklift"
     },
     {
-        "productName": "Duck",
-        "fileName": "12248_Bird_v1_L2"
+        "productName": "T-Bone Steak",
+        "fileName": "10879_Cooked Steak (T-bone)_v1_L3"
     },
     {
-        "productName": "Turtle",
-        "fileName": "10042_Sea_Turtle_V2_iterations-2"
+        "productName": "Ammonite Fossil",
+        "fileName": "13634_AmmoniteFossil_v1_l2"
     },
-    {
-        "productName": "Camera",
-        "fileName": "10124_SLR_Camera_SG_V1_Iteration2"
-    },
-    {
-        "productName": "Dolphin",
-        "fileName": "10014_dolphin_v2_max2011_it2"
-    },
-    {
-        "productName": "Monkey",
-        "fileName": "12958_Spider_Monkey_v1_l2"
-    },
-    {
-        "productName": "Bicycle",
-        "fileName": "11717_bicycle_v2_L1"
-    }
 ]
 
 export default productsFileNamesMapping;
